0329ac35a22346b9fbc1dff80c7a2101b077fd6b
Paul Stettnisch <paulstettnisch@gmail.com>
2017-05-19 16:31:19 -0500

    Add JS engine & rubyracer

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
 
   def homepage
-    render text: "THIS IS FROM A RAILS APP SUCKA"
+    render text: "THIS IS FROM A RAILS APP on AWS EC2 SUCKA"
   end
 end
